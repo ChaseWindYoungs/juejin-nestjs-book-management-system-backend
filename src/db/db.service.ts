ee78f5a7838add130340455cafd1d39e43565c0b
chasewindyoungs <chasewindyoungs@163.com>
2024-08-05 17:51:32 +0800

    feat: init

---
+++ src/db/db.service.ts
@@ -0,0 +1,30 @@
+import { access, readFile, writeFile } from 'fs/promises';
+import { DbModuleOptions } from './db.module';
+import { Inject, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class DbService {
+  @Inject('OPTIONS')
+  private options: DbModuleOptions;
+  async read() {
+    const filePath = this.options.path;
+
+    try {
+      // 测试用户对 path 指定的文件或目录的权限。
+      await access(filePath);
+    } catch (error) {
+      return [];
+    }
+
+    const str = await readFile(filePath, { encoding: 'utf8' });
+    if (!str) return [];
+
+    return JSON.parse(str);
+  }
+  async write(obj: Record<string, any>) {
+    const filePath = this.options.path;
+    await writeFile(filePath, JSON.stringify(obj, null, 2), {
+      encoding: 'utf8',
+    });
+  }
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,49 @@
+import { RegisterUserDto } from './dto/register-user.dto';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { DbService } from 'src/db/db.service';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UserService {
+  @Inject(DbService)
+  dbService: DbService;
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+
+  async register(registerUserDto: RegisterUserDto) {
+    const users: User[] = await this.dbService.read();
+    const foundUser = users.find(
+      (user) => user.username === registerUserDto.username,
+    );
+    if (foundUser) {
+      throw new BadRequestException('User already exists');
+    }
+
+    const user = new User();
+    user.username = registerUserDto.username;
+    user.password = registerUserDto.password;
+    users.push(user);
+
+    await this.dbService.write(users);
+    return user;
+  }
+}
